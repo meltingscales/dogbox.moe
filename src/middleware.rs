719cbae7997d4d8d5a49b8490e55c03470ef2b28
Henry Post <HenryFBP@gmail.com>
2025-11-08 13:06:42 -0600

    fix CSP hash - remove extra equals sign

--- src/middleware.rs
+++ src/middleware.rs
@@ -51,7 +51,7 @@ pub async fn security_headers(
                'sha256-SGWGvJu8HcqulHmTV7/WfP/TjcWCtNH40zGJbhmurLQ=' \
                'sha256-UwIxe9p9b2FNZcGBE29ru4ohO+xC1LiOPTC/1s6DRDI=' \
                'sha256-dOFOu+c3tOHIxiHjp4NQ7kBAJNPVqIV2C0nsVeEtLZU=' \
-               'sha256-gXFFdg/UCt0MfJH9IbdwGFsCxpsE4aa9D0vNkYyzRcA=='; \
+               'sha256-gXFFdg/UCt0MfJH9IbdwGFsCxpsE4aa9D0vNkYyzRcA='; \
              style-src 'self' 'unsafe-inline'; \
              img-src 'self' data: blob:; \
              media-src 'self' blob:; \
